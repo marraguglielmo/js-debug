f2bd8954737c99b657e439ab7b85e1dfcb934c76
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-03-08 16:06:20 +0100

    [add] esercizio 4

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -56,11 +56,24 @@ loopToFive();
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
         }
-        return evenNumbers;
     }
+    return evenNumbers;
 }
-displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+console.log(displayEvenNumbers()); // dovrebbe restituire [2,4,6,8]
+// RISOLUZIONE /////////////
+/*
+1. questa funzione cicla l'array numbers, verifica se il numero è pari, se è vero pusha il numero nell array evenNumbers, altrimenti restituisce l array evenNumbers
+2. l'errore di sintassi sono: 
+    -il punto e virgola nel ciclo for
+    - = invede di === nella condizione
+    - il punto e virgoila fuori dalla condizione
+3. gli errori logici sono:
+    - numbers.length invece di numbers.length -1
+    -nella condizione numbers[i] invece di numbers, altrimenti prende tutto l array
+    -lo stesso nel push, perchè devo pushare il numero a indice i
+    -il return deve stare fuori dal ciclo for, altrimenti il ciclo si blocca alla prima iterazione
+*/
